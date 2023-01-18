2f529519b206afb2cf0a21989c2a8983361743e3
Dayan1533 <dayanramos5537@gmail.com>
2023-01-18 17:39:27 -0300

    Add comments

--- contatos/models.py
+++ contatos/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+# A função timezone.now é utilizada como o valor padrão, o que significa que, 
+# se você não especificar um valor para esse campo ao criar um novo registro, 
+# a data e hora atuais serão automaticamente atribuídas a ele.
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
@@ -15,6 +18,7 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    # on_delete=models.DO_NOTHING, faz com que, quando a categoria for apagada, seus Contatos ligados não façam nada.
 
-    # def __str__(self): 
-    #     return self.nome
+    def __str__(self): 
+        return self.nome
