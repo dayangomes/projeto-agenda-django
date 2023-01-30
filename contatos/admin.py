b345918a587f2e7aa6dde50f051e51593f94280b
Dayan1533 <dayanramos5537@gmail.com>
2023-01-30 15:58:08 -0300

    add list editable

--- contatos/admin.py
+++ contatos/admin.py
@@ -7,16 +7,17 @@ admin.site.site_header = 'Agenda Administrador'
 
 # Exibindo mais campos na listagem de contatos
 class ContadoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
     # Exibindo os campos que serão links
     list_display_links = ('id', 'nome', 'sobrenome')
-
     #  Exibindo os campos que serão usados para filtrar a listagem
     # list_filter = ('nome', 'sobrenome')
     # mostra 10 contatos por página
     list_per_page = 8
     # Exibe um campo de busca
     search_fields = ('nome', 'sobrenome', 'telefone')
+    # Torna os campos listados editáveis
+    list_editable = ('telefone', 'mostrar')
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContadoAdmin)
