063102ea817113683c6cc4f7e1688a659e164800
Dayan1533 <dayanramos5537@gmail.com>
2023-01-31 17:04:49 -0300

    adding pagination

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+# from django.http import Http404
+from django.core.paginator import Paginator
 from .models import Contato
 
 def index(request):
     contatos = Contato.objects.all()
+
+    paginator = Paginator(contatos, 5) # Show 5 contacts per page.
+    page = request.GET.get('p') # p é o nome do parâmetro que será passado na url
+    contatos = paginator.get_page(page) # contatos é o nome da variável que será passada para o template
+
+
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
