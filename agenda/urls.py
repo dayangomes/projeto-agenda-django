7dfbbf9e1f5881264d4f38a8b0308e5d64941019
Dayan1533 <dayanramos5537@gmail.com>
2023-02-13 17:12:04 -0300

    Build: Configuring new app

--- agenda/urls.py
+++ agenda/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static # Importa a função static
 
 urlpatterns = [
     path('', include('contatos.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Configuração para o upload de arquivos
